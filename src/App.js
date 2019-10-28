7a430307fb0ace63f0f90bd5dcac10ef9720ee7e
Oliver Sun <olisun@mac.com>
2019-10-27 21:24:15 -0700

    built navbar and footer. set-up base routes

--- src/App.js
+++ src/App.js
@@ -1,34 +1,64 @@
 import React, { Component } from "react";
-import Header from "./components/Header";
-import Portfolio from "./components/Portfolio";
-import projects from "./projects.json";
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import Container from "react-bootstrap/Container";
+import NavBar from "react-bootstrap/NavBar";
+import Nav from "react-bootstrap/Nav";
 import "./App.css";
 
+import Footer from "./components/Footer";
+
 class App extends Component {
-  state = {
-    projects,
+  
+  constructor(props) {
+    super(props);
+    this.state={
+      title: "Oliver Sun",
+      headerLinks: [
+        { title: "Home", path: "/" },
+        { title: "About", path: "/about" },
+        { title: "Contact", path: "/contact" }
+      ],
+      home: {
+        title: "You're never too old!",
+        subTitle: "Featured Projects",
+        text: "Checkout my projects"
+      },
+      about: {
+        title: "About Me"
+      },
+      contact: {
+        title: "You can reach me any time!"
+      }
+    }
   }
 
   render() {
     return (
-      <div className="container">
-        <div>
-          <Header />
-        </div>
-        <div className="row">
-          {this.state.projects.map(project => (
-            <Portfolio
-              key={project.id}
-              id={project.id}
-              title={project.title}
-              link={project.link}
-              image={project.image}
-             />
-          ))}
-        </div>
-      </div>
+      <Router>
+        <Container className="p-0" fluid={true}>
+          <NavBar className="border-bottom" bg="transparent" expand="lg">
+            <NavBar.Brand>Oliver Sun</NavBar.Brand>
+            <NavBar.Toggle className="border-0" aria-controls="navbar-toggle" />
+            <NavBar.Collapse id="navbar-toggle">
+              <Nav className="ml-auto">
+                <Link className="nav-link" to="/">Home</Link>
+                <Link className="nav-link" to="/about">About Me</Link>
+                <Link className="nav-link" to="/contact">Contact Me</Link>
+              </Nav>
+            </NavBar.Collapse>
+          </NavBar>
+
+          <Footer />
+
+        </Container>
+
+      </Router>
     )
   }
 }
+
+
+
+
 
 export default App;
